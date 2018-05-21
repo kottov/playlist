00724924a6ed7befbef95a384ee937df4a7db066
kottov <kottov@gmail.com>
2018-05-22 00:01:34 +0300

    Simple web server which returns profile

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
+type Profile struct {
+	Name    string
+	Hobbies []string
+}
+
 func main() {
-	fmt.Println("Hello!")
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":8080", nil)
+	log.Println("Server started!")
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	log.Println("Request recieved!")
+	profile := Profile{"Kostya", []string{"music", "codding"}}
+	log.Printf("Profile created: %s", profile)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(profile)
+	log.Printf("Response sent.")
 }
